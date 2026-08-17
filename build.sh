0ce389507d6c2cd2530d66ec45dc9bd422918303
dark <dark@localhost>
2026-08-17 13:44:08 +0300

    darkc name.dark: compile+run by default; -o for compile-only; add exec builtin (Linux execve, Windows WinExec); remove .sh .bat

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Self-hosting bootstrap: rebuild darkc and darkc.exe from their own Dark source.
-set -e
-./darkc examples/darkc.dark ./darkc.new
-chmod +x ./darkc.new
-mv ./darkc.new ./darkc
-./darkc examples/darkc.dark ./darkc.exe --target windows
-echo "darkc and darkc.exe rebuilt"
